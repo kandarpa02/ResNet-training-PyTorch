2830695a157584f5b78a57e1de8d4aa6c8df278a
kandarpa02 <kandarpaexe@gmail.com>
2026-01-12 17:26:04 +0530

    setup done

--- resnet_training/train.py
+++ resnet_training/train.py
@@ -94,8 +94,9 @@ def trainer(
           optimizer.zero_grad(set_to_none=True)
 
           train_loss, train_acc = batched_train_step(x, y, auto_cast=auto_cast)
-          t_loss.update(train_loss.item())
-          t_acc.update(train_acc.item())
+          b_size = x.size(0)
+          t_loss.update(train_loss.item(), b_size)
+          t_acc.update(train_acc.item(), b_size)
 
           if auto_cast:
             scalar.scale(train_loss).backward()
@@ -109,8 +110,9 @@ def trainer(
           x = x.to(device)
           y = y.to(device)
           val_loss, val_acc = batched_val_step(x, y, auto_cast=auto_cast)
-          v_loss.update(val_loss.item())
-          v_acc.update(val_acc.item())
+          b_size = x.size(0)
+          v_loss.update(val_loss.item(), b_size)
+          v_acc.update(val_acc.item(), b_size)
 
 
       t1 = time.time()
